aa5508e667a8fb7b344ba793cc8b7069bc20ac51
Oleg Sazonov <oleg.sazonov.work@gmail.com>
2024-11-06 17:21:51 +0200

    feat(sliders.js): add main and feedback sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 'use strict';
 
 import modals from './modules/modals';
+import sliders from './modules/sliders';
 
 window.addEventListener('DOMContentLoaded', () => {
 
 	modals();
+	sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+	sliders('.main-slider-item', 'vertical', '', '');
 });
