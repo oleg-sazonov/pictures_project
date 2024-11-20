8a86d88d460258b425b24c311be45d6c19a8a257
Oleg Sazonov <oleg.sazonov.work@gmail.com>
2024-11-20 16:04:31 +0200

    refactor(pictureSize.js): update events for touch-screens

--- src/js/modules/pictureSize.js
+++ src/js/modules/pictureSize.js
@@ -5,27 +5,34 @@ const pictureSize = (imgSelector) => {
 
 	function showImg(block) {
 		const img = block.querySelector('img');
-		// img.src = img.src.slice(0, -4) + '-1.png';
 		img.src = img.src.replace(/(sizes-\d)(\.png)/, '$1-1$2');
 		block.querySelectorAll('p:not(.sizes-hit)').forEach(item => item.style.display = 'none');
 	};
 
 	function hideImg(block) {
 		const img = block.querySelector('img');
-		// img.src = img.src.slice(0, -6) + '.png';
 		img.src = img.src.replace(/-1\.png$/, '.png');
 		block.querySelectorAll('p:not(.sizes-hit)').forEach(item => item.style.display = 'block');
 	};
 
-	blocks.forEach(block => {
-		block.addEventListener('mouseover', () => showImg(block));
-	});
+	let activeBlock = null;
 
 	blocks.forEach(block => {
-		block.addEventListener('mouseout', () => hideImg(block));
-	});
+		const isTouchDevice = 'ontouchstart' in window;
 
-	
+		if (isTouchDevice) {
+			block.addEventListener('touchstart', () => {
+				if (activeBlock && activeBlock !== block) {
+					hideImg(activeBlock); 
+				}
+				showImg(block); 
+				activeBlock = block; 
+			});
+		} else {
+			block.addEventListener('mouseover', () => showImg(block));
+			block.addEventListener('mouseout', () => hideImg(block));
+		}
+	});
 };
 
 export default pictureSize;
